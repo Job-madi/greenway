52b3b47b9ca999a73077ea5e0837af42dc2dabbb
Hanna Sabu Mampallil <68439953+hanna2001@users.noreply.github.com>
2021-03-28 03:11:19 +0530

    update aqiscreen

--- lib/aqiscreen.dart
+++ lib/aqiscreen.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+import 'package:air_quality/air_quality.dart';
 
 class aqiscreen extends StatefulWidget {
   @override
@@ -7,8 +11,27 @@ class aqiscreen extends StatefulWidget {
 }
 
 class _aqiscreenState extends State<aqiscreen> {
-  String country;
-  String city;
+  //String _key = 'de113a567839c4ae761b35831192453ae5f57551';
+  AirQuality airQuality =
+      new AirQuality('de113a567839c4ae761b35831192453ae5f57551');
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getData(double lat, double lon) async {
+    AirQualityData feedFromGeoLocation =
+        await airQuality.feedFromGeoLocation(lat, lon);
+    setState(() {
+      aqi = feedFromGeoLocation.airQualityIndex.toString();
+    });
+  }
+
+  final _controller = TextEditingController();
+  double lat;
+  double lon;
+  String aqi = '';
+  String city = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,36 +54,39 @@ class _aqiscreenState extends State<aqiscreen> {
             ),
             Container(
               padding: EdgeInsets.all(20.0),
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  //hoverColor: Colors.black,
-                  filled: true,
-                  fillColor: Colors.black,
-                  hintText: 'Enter Country',
-                  hintStyle: TextStyle(color: Colors.white),
-                ),
-                onChanged: (value) {
-                  country = value;
-                },
-              ),
+              child: GooglePlaceAutoCompleteTextField(
+                  textEditingController: _controller,
+                  googleAPIKey: "AIzaSyDyCqyYlxIkhhtDAzoCgwqlfkVTwGYDIo4",
+                  inputDecoration: InputDecoration(
+                    //hoverColor: Colors.black,
+                    filled: true,
+                    //fillColor: Colors.black,
+                    hintText: 'Enter City',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  debounceTime:
+                      800, // default 600 ms, // optional by default null is set
+                  isLatLngRequired:
+                      true, // if you required coordinates from place detail
+                  getPlaceDetailWithLatLng: (Prediction prediction) {
+                    // this method will return latlng with place detail
+
+                    print("placeDetails " +
+                        prediction.lat.toString() +
+                        " " +
+                        prediction.lng.toString());
+                    lat = double.parse(prediction.lat);
+                    lon = double.parse(prediction.lng);
+                    city = prediction.description;
+                    getData(lat, lon);
+                  }, // this callback is called when isLatLngRequired is true
+                  itmClick: (Prediction prediction) {
+                    _controller.text = prediction.description;
+                    _controller.selection = TextSelection.fromPosition(
+                        TextPosition(offset: prediction.description.length));
+                  }),
             ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  //hoverColor: Colors.black,
-                  filled: true,
-                  fillColor: Colors.black,
-                  hintText: 'Enter City',
-                  hintStyle: TextStyle(color: Colors.white),
-                ),
-                onChanged: (value) {
-                  city = value;
-                },
-              ),
-            ),
+            Center(child: Text('AQI :' + aqi))
           ],
         ),
       ),
